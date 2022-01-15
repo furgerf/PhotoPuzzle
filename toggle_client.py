6ccd06ca88078a595387504a70c4e1fadf3c01bb
Fabian Furger <furgerfabian@hotmail.com>
2022-01-15 15:36:37 +0100

    Add toggling client

---
+++ toggle_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from random import random
+from time import sleep
+
+import requests
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--column", type=int)
+    parser.add_argument("--row", type=int)
+    args = parser.parse_args()
+
+    while True:
+        print(f"Toggling {args.column}/{args.row}")
+        requests.get(f"http://localhost:8000/api/image/column/{args.column}/row/{args.row}/state/toggle")
+        sleep(15 * random())
+
+
+if __name__ == "__main__":
+    main()
