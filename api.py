7e74e351f5ab424acb676455758c1de19e341c49
Fabian Furger <furgerfabian@hotmail.com>
2022-01-15 13:43:00 +0100

    Compose image of many small images

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from time import time
 
 import numpy as np
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from PIL import Image
 
@@ -15,6 +16,12 @@ logging.basicConfig(
 logger = logging.getLogger("api")
 
 app = FastAPI()
+api = FastAPI()
+app.mount("/api", api)
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
 
 
 @app.middleware("http")
@@ -25,12 +32,7 @@ async def log_process_time(request: Request, call_next):
     return response
 
 
-rows = int(os.environ["ROWS"])
-columns = int(os.environ["COLUMNS"])
-image_states = np.full((rows, columns), 0)
-
 resize = int(os.environ["RESIZE"])
-
 
 images = []
 for image_file in glob(os.path.join("data", "*.png")):
@@ -39,21 +41,47 @@ for image_file in glob(os.path.join("data", "*.png")):
         new_size = (img.size[0] // resize, img.size[1] // resize)
         logger.info("Resizing by %d (from %s to %s)", resize, img.size, new_size)
         image = img.resize(new_size)
-        images.append(np.array(image))
+        images.append(np.swapaxes(image, 0, 1))
+
+columns = int(os.environ["COLUMNS"])
+rows = int(os.environ["ROWS"])
+image_states = np.random.randint(len(images), size=(columns, rows))
 
 
-def construct_image():
-    logger.info("Constructing current image")
-    result = images[0]
-    logger.info("%s %s", result.dtype, result.shape)
-    return result
+def _get_image_tile(column: int, row: int):
+    logger.info("Getting tile %d/%d", column, row)
+    image = images[image_states[column, row]]
+    column_width = image.shape[0] // columns
+    row_height = image.shape[1] // rows
+    return image[column * column_width : (column + 1) * column_width, row * row_height : (row + 1) * row_height]
 
 
-@app.get("/image/full")
-def get_full_image() -> StreamingResponse:
-    image = Image.fromarray(construct_image(), mode="RGB")
+def _get_full_image():
+    image_array = np.empty_like(images[0])
+    column_width = image_array.shape[0] // columns
+    row_height = image_array.shape[1] // rows
+    for column in range(columns):
+        for row in range(rows):
+            image_array[
+                column * column_width : (column + 1) * column_width, row * row_height : (row + 1) * row_height
+            ] = _get_image_tile(column, row)
+    return image_array
+
+
+def encode_image(image_array):
+    image = Image.fromarray(np.swapaxes(image_array, 0, 1), mode="RGB")
     logger.info("Encoding image")
     output = io.BytesIO()
     image.save(output, format="PNG")
     output.seek(0)
     return StreamingResponse(output, media_type="image/png")
+
+
+@api.get("/image/column/{column}/row/{row}")
+def get_image_tile(column: int, row: int) -> StreamingResponse:
+    return encode_image(_get_image_tile(column, row))
+
+
+@api.get("/image/full")
+def get_full_image() -> StreamingResponse:
+    return encode_image(_get_full_image())
